2584f5c92ccdbf9f7c30c2bb34d2818595e7f59d
Oromion <caznaranl@uni.pe>
2018-12-22 08:15:31 -0500

    Update

---
+++ motivation.md
@@ -0,0 +1,8 @@
+* C++ no es un lenguaje único de la industria.
+* C++ emplea los paradigmas de programación estructurada y orientado a objetos.
+
+Para ser un buen programador, es necesario preparar diversos programas.
+
+El lenguaje de programación es la evolución del lenguaje C.
+
+C++ viene equipada con una poderosa biblioteca, la Standard Template Library
---
+++ temp.cpp
@@ -0,0 +1,20 @@
+// Copyleft 2018
+#include <float.h>
+#include <stdarg.h>
+
+// Let's try typing while
+int main(int argc, char *argv[])
+{
+  int variable_index = 0;
+  int hello_world = 10;
+  while (variable_index !=10){
+    printf("This is good %d", hello_world);
+    variable_index++;
+  }
+
+  for (int i = 20; i < 100; ++i) {
+      printf("yes") 
+  }
+  return 0;
+}
+ 
